3435e8758287ff3eeb0283cdce0e1cbd6f833967
Mihail Kiyamov <mihailkiyamov@yandex.ru>
2023-05-05 16:25:04 +0500

    Sprint10-Try1

---
+++ src/Components/App.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
+import PopupWithImage from './PopupWithImage';
+
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setSelectedCard({});
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  }
+
+  return (
+    <div className="App">
+      <body className="page">
+        <div className="page__container">
+          <Header></Header>
+          <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}></Main>
+          <Footer></Footer>
+        </div>
+        <PopupWithForm name='edit-profile' title='Редактировать профиль' isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} children={<> <label className="popup__form-field">
+          <input className="popup__input popup__input_type_name" type="text" id="name-input" name="name"
+            placeholder="Ваше имя" required minLength="2" maxLength="40" />
+          <span className="popup__input-error name-input-error"></span>
+        </label>
+          <label className="popup__form-field">
+            <input className="popup__input popup__input_type_about" type="text" id="about-input" name="about"
+              placeholder="О вас" required minLength="2" maxLength="200" />
+            <span className="popup__input-error about-input-error"></span>
+          </label> </>}></PopupWithForm>
+        <PopupWithForm name='add-element' title='Новое место' isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} children={<> <label className="popup__form-field">
+          <input className="popup__input popup__input_type_mesto-name" type="text" id="mesto-name-input"
+            name="mestoName" placeholder="Название" minLength="2" maxLength="30" required />
+          <span className="popup__input-error mesto-name-input-error"></span>
+        </label>
+          <label className="popup__form-field">
+            <input className="popup__input popup__input_type_mesto-src" type="url" id="mesto-src-input"
+              name="mestoSrc" placeholder="Ссылка на картинку" required />
+            <span className="popup__input-error mesto-src-input-error"></span>
+          </label> </>}></PopupWithForm>
+        <PopupWithImage card={selectedCard} onClose={closeAllPopups}></PopupWithImage>
+        <PopupWithForm name='warning' title='Вы уверены?' isOpen={false} onClose={closeAllPopups}></PopupWithForm>
+        <PopupWithForm name='change-avatar' title='Обновить аватар' isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} children={<><label className="popup__form-field">
+          <input className="popup__input popup__input_type_avatar-src" type="url" id="avatar-src-input"
+            name="avatarSrc" placeholder="Ссылка на картинку" required />
+          <span className="popup__input-error avatar-src-input-error"></span>
+        </label></>}></PopupWithForm>
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import api from '../utils/api';
+import Card from './Card';
+
+function Main(props) {
+    const [userName, setUserName] = useState();
+    const [userDescription, setUserDescription] = useState();
+    const [userAvatar, setUserAvatar] = useState();
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        api.getProfileInfo()
+            .then((data) => {
+                setUserName(data.name);
+                setUserDescription(data.about);
+                setUserAvatar(data.avatar);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+
+    useEffect(() => {
+        api.getInitialCards()
+            .then((data) => {
+                setCards(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+
+    return (
+        <main className="content">
+            <section className="profile">
+                <div className="profile__circle-for-image" onClick={props.onEditAvatar}>
+                    <img className="profile__avatar" src={userAvatar} alt="Аватар пользователя" />
+                </div>
+                <div className="profile__info">
+                    <h1 className="profile__name">{userName}</h1>
+                    <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
+                    <p className="profile__about">{userDescription}</p>
+                </div>
+                <button className="profile__add-button" type="button" onClick={props.onAddPlace}></button>
+            </section>
+            <section className="elements">
+                {cards.map((cardObj) => (<Card card={cardObj} onCardClick={props.onCardClick}></Card>))}
+            </section>
+        </main>
+    );
+}
+
+export default Main;
